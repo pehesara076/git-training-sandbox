da4a680ae7b5cc674dbb91bdb4e19f1618689b7c
Pasindu Weerasekara <Pehesaratech1977@gmail.com>
2026-07-11 03:16:44 +0530

    Update registry.js

--- registry.js
+++ registry.js
@@ -1,2 +1,2 @@
-const students = ["Pasindu pehesara","moda sahan","Malith sahan"];
+const students = ["Pasindu pehesara","moda sahan","Malith sahan","gon haraka"];
 console.log("current Registry:", students);
