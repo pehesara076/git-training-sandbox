be5220148e9fde9cdaa04cc7bf8d75d3e5209da5
Sahan012 <malithsahan01@gmail.com>
2026-07-12 17:16:21 +0530

    malith change

--- registry.js
+++ registry.js
@@ -1,8 +1,9 @@
-const students = ["Pasindu pehesara","moda sahan","Malith sahan","gon haraka"];
+const students = ["Pasindu pehesara","moda sahan","Malith sahan","gon haraka","pahasara modaya"];
 console.log("current Registry:", students);
 console.log("Pratheep");
 console.log("Pratheep");
 console.log("Pratheep");
 console.log("Pratheep");
 console.log("Pratheep");
+
 
